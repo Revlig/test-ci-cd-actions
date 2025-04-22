e62d4cbc52933d572b58fc085b607ccfa8527209
gilver_souza <gilver@localhost.localdomain>
2025-04-22 00:29:07 -0300

    Add path url

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,11 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/test-ci-cd-actions/',
   plugins: [react()],
   test: {
     globals: true,
     environment: 'jsdom',
-    setupFiles: './vitest.setup.ts', // <- aqui
+    setupFiles: './vitest.setup.ts',
   },
 })
